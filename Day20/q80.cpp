032f36451c151564b542ac4390fe5d2df4f882c1
Raghav <lost.air.kido@gmail.com>
2026-06-17 10:05:59 +0530

    uploading day 20 program

---
+++ Day20/q80.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+
+    int n;
+
+    cout<<"enter the size of matrix : ";
+    cin>>n;
+
+    int mat1[n][n];
+
+    cout<<"enter matrix"<<endl;
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>mat1[i][j];
+        }
+    }
+
+
+
+    for(int i=0;i<n;i++){
+        int sum=0;
+        for(int j=0;j<n;j++){
+            sum+=mat1[j][i];
+        }
+        cout<<"sum of "<<i+1<<" column is "<<sum<<endl;
+    }
+
+
+
+    return 0;
+
+}
